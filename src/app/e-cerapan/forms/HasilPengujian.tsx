602e511746d106a5fc06cebfc44f557c57b293a3
Hilmi Mithwa <hilmimithwa@gmail.com>
2026-09-24 13:17:51 +0700

    fix: moving HasilPengujian Components into desire folder

--- src/app/e-cerapan/forms/HasilPengujian.tsx
+++ src/app/e-cerapan/forms/HasilPengujian.tsx
@@ -14,6 +14,7 @@ import MetricSummary from "@/components/e-cerapan/cards/MetricSummary";
 import InfoCard from "@/components/e-cerapan/cards/InfoCard";
 import ParameterTable from "@/components/e-cerapan/table/ParameterTable";
 import ConfirmationModal from "@/components/e-cerapan/modal/ConfirmationModal";
+<<<<<<< HEAD
 =======
 import PageHeader from "@/components/e-cerapan/layout/PageHeader";
 import ResultCard from "@/components/e-cerapan/ui/ResultCard";
@@ -22,6 +23,8 @@ import InfoCard from "@/components/e-cerapan/ui/InfoCard";
 import ParameterTable from "@/components/e-cerapan/ui/ParameterTable";
 import ConfirmationModal from "@/components/e-cerapan/ui/ConfirmationModal";
 >>>>>>> 71c9bb6 (apply atomic design and fix HasilPemeriksaanAwal, PemeriksaanAwal, PengujianPerhitungan, page e-cerapan (belum semua selesai, cek aja dulu))
+=======
+>>>>>>> 30f0336 (fix: moving HasilPengujian Components into desire folder)
 import { WizardStepProps } from "@/types/wizard";
 import StatCard from "@/components/e-cerapan/cards/StatCard";
 
