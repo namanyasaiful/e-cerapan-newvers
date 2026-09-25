3d05d14877c759570f0b1c6eadbebf7889f190e1
namanyasaiful <saifullohfattah023@gmail.com>
2026-09-25 10:15:40 +0700

    fix: conflict message at code

--- src/app/e-cerapan/forms/HasilPengujian.tsx
+++ src/app/e-cerapan/forms/HasilPengujian.tsx
@@ -7,24 +7,13 @@ import autoTable from "jspdf-autotable";
 
 import Breadcrumb from "@/components/e-cerapan/layout/Breadcrumb";
 import Stepper from "@/components/e-cerapan/layout/Stepper";
-<<<<<<< HEAD
+
 import PageHeader from "@/components/e-cerapan/ui/PageHeader";
 import ResultCard from "@/components/e-cerapan/cards/ResultCard";
 import MetricSummary from "@/components/e-cerapan/cards/MetricSummary";
 import InfoCard from "@/components/e-cerapan/cards/InfoCard";
 import ParameterTable from "@/components/e-cerapan/table/ParameterTable";
 import ConfirmationModal from "@/components/e-cerapan/modal/ConfirmationModal";
-<<<<<<< HEAD
-=======
-import PageHeader from "@/components/e-cerapan/layout/PageHeader";
-import ResultCard from "@/components/e-cerapan/ui/ResultCard";
-import MetricSummary from "@/components/e-cerapan/ui/MetricSummary";
-import InfoCard from "@/components/e-cerapan/ui/InfoCard";
-import ParameterTable from "@/components/e-cerapan/ui/ParameterTable";
-import ConfirmationModal from "@/components/e-cerapan/ui/ConfirmationModal";
->>>>>>> 71c9bb6 (apply atomic design and fix HasilPemeriksaanAwal, PemeriksaanAwal, PengujianPerhitungan, page e-cerapan (belum semua selesai, cek aja dulu))
-=======
->>>>>>> 30f0336 (fix: moving HasilPengujian Components into desire folder)
 import { WizardStepProps } from "@/types/wizard";
 import StatCard from "@/components/e-cerapan/cards/StatCard";
 
